bbbb81792bfbb5c0bf3a19fdfc0bba3a7541acba
oskar <oskar.bahr.lg@gmail.com>
2025-03-25 22:34:49 +0100

    Add auth to open nomination page

--- src/app/dashboard/open/[urlId]/page.tsx
+++ src/app/dashboard/open/[urlId]/page.tsx
@@ -1,7 +1,8 @@
 import { db } from "@/server/db";
 import { nominationRequestsTable } from "@/server/db/schema";
-import { eq } from "drizzle-orm";
+import { eq, and } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { auth, signIn } from "@/server/auth";
 
 export default async function OpenStatePage({
   params,
@@ -9,11 +10,22 @@ export default async function OpenStatePage({
   params: Promise<{ urlId: string }>;
 }) {
   const { urlId } = await params;
+  const session = await auth();
+
+  if (!session?.user) {
+    await signIn();
+    return;
+  }
 
   const nominationRequest = await db
     .select()
     .from(nominationRequestsTable)
-    .where(eq(nominationRequestsTable.urlId, urlId));
+    .where(
+      and(
+        eq(nominationRequestsTable.urlId, urlId),
+        eq(nominationRequestsTable.traktUserId, session.user.id),
+      ),
+    );
 
   if (!(nominationRequest[0]?.state === "open"))
     return redirect(`/dashboard/closed/${urlId}`);
