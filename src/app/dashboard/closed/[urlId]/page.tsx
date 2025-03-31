7658fa4b6cd1fbf427a9a8cc65dd042f2279893e
oskar <oskar.bahr.lg@gmail.com>
2025-03-31 22:11:34 +0200

    Add auth check to closed nomination request page

--- src/app/dashboard/closed/[urlId]/page.tsx
+++ src/app/dashboard/closed/[urlId]/page.tsx
@@ -1,9 +1,17 @@
+import { auth, signIn } from "@/server/auth";
+
 export default async function ClosedStatePage({
   params,
 }: {
   params: Promise<{ urlId: string }>;
 }) {
   const { urlId } = await params;
+  const session = await auth();
+
+  if (!session?.user) {
+    await signIn();
+    return;
+  }
 
   return <div>This Nomination Request is closed: UrlId: {urlId}</div>;
 }
