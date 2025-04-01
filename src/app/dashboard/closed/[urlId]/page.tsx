60f460572a34e2f8a564fd24f4374c9e09d761e4
oskar <oskar.bahr.lg@gmail.com>
2025-04-01 21:53:29 +0200

    Add db request to nominations closed page

--- src/app/dashboard/closed/[urlId]/page.tsx
+++ src/app/dashboard/closed/[urlId]/page.tsx
@@ -1,4 +1,8 @@
 import { auth, signIn } from "@/server/auth";
+import { db } from "@/server/db";
+import { eq, and } from "drizzle-orm/expressions";
+import { nominationRequestsTable } from "@/server/db/schema";
+import { redirect } from "next/navigation";
 
 export default async function ClosedStatePage({
   params,
@@ -13,5 +17,24 @@ export default async function ClosedStatePage({
     return;
   }
 
-  return <div>This Nomination Request is closed: UrlId: {urlId}</div>;
+  const nominationRequest = await db
+    .select()
+    .from(nominationRequestsTable)
+    .where(
+      and(
+        eq(nominationRequestsTable.urlId, urlId),
+        eq(nominationRequestsTable.traktUserId, session.user.id),
+      ),
+    );
+
+  if (!nominationRequest[0]) return redirect("/dashboard");
+  if (!(nominationRequest[0].state === "closed"))
+    return redirect(`/dashboard/open/${urlId}`);
+
+  return (
+    <div>
+      This Nomination Request is closed: UrlId: {urlId} It has the name:{" "}
+      {nominationRequest[0].name}
+    </div>
+  );
 }
