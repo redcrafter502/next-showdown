4f488da6f31b3e90c62089f77f82da1d433747d7
oskar <76687828+redcrafter502@users.noreply.github.com>
2025-04-08 09:19:48 +0200

    Display all nominations on closed nomination request page

--- src/app/dashboard/closed/[urlId]/page.tsx
+++ src/app/dashboard/closed/[urlId]/page.tsx
@@ -1,8 +1,15 @@
 import { auth, signIn } from "@/server/auth";
 import { db } from "@/server/db";
 import { eq, and } from "drizzle-orm/expressions";
-import { nominationRequestsTable } from "@/server/db/schema";
+import {
+  nominationRequestsTable,
+  nominationsTable,
+  seasonsTable,
+} from "@/server/db/schema";
 import { redirect } from "next/navigation";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Check, X } from "lucide-react";
 
 export default async function ClosedStatePage({
   params,
@@ -25,16 +32,73 @@ export default async function ClosedStatePage({
         eq(nominationRequestsTable.urlId, urlId),
         eq(nominationRequestsTable.traktUserId, session.user.id),
       ),
+    )
+    .innerJoin(
+      nominationsTable,
+      eq(nominationRequestsTable.id, nominationsTable.nominationRequestId),
+    )
+    .innerJoin(
+      seasonsTable,
+      eq(nominationsTable.nominatedSeasonId, seasonsTable.id),
     );
 
   if (!nominationRequest[0]) return redirect("/dashboard");
-  if (!(nominationRequest[0].state === "closed"))
+  if (!(nominationRequest[0].nominationRequests.state === "closed"))
     return redirect(`/dashboard/open/${urlId}`);
+
+  const nominations = nominationRequest.flatMap((nominationRequest) => {
+    const count = nominationRequest.nominations.count;
+    if (count === 0) return [];
+
+    const returnNominations = [];
+
+    for (let i = 0; i < count; i++) {
+      returnNominations.push(nominationRequest);
+    }
+
+    return returnNominations;
+  });
 
   return (
     <div>
       This Nomination Request is closed: UrlId: {urlId} It has the name:{" "}
-      {nominationRequest[0].name}
+      {nominationRequest[0].nominationRequests.name}
+      <div className="flex flex-wrap gap-4">
+        {nominations.map((n, index) => (
+          <Nomination key={index} nominations={n} index={index} />
+        ))}
+      </div>
     </div>
   );
+}
+
+function Nomination({
+  nominations: { nominationRequests, seasons, nominations },
+  index,
+}: {
+  nominations: {
+    nominationRequests: typeof nominationRequestsTable.$inferSelect;
+    seasons: typeof seasonsTable.$inferSelect;
+    nominations: typeof nominationsTable.$inferSelect;
+  };
+  index: number;
+}) {
+  if (nominations.state === "open")
+    return (
+      <Card>
+        <CardHeader>
+          <div className="flex flex-row items-center gap-4">
+            <Button variant="secondary">
+              <X color="red" />
+            </Button>
+            <span className="text-xl">{index + 1}</span>
+            <Button variant="secondary">
+              <Check color="green" />
+            </Button>
+          </div>
+        </CardHeader>
+      </Card>
+    );
+  if (nominations.state === "nominated") return <Card>Nominated</Card>;
+  if (nominations.state === "revoked") return <Card>Revoked</Card>;
 }
